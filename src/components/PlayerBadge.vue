b3bc2fa5c37545e895262aa8a95bf0f8f21edb2d
Piero del Valle <devapeu@gmail.com>
2025-04-22 09:08:30 -0400

    remove defineProps

--- src/components/PlayerBadge.vue
+++ src/components/PlayerBadge.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, defineProps } from 'vue';
+import { computed } from 'vue';
 
 function getGodIcon(name) {
   return new URL(`../assets/gods/${name}_icon.avif`, import.meta.url).href;
